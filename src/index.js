654fca910ac7ff800646eeba66f2a0e52a8445f3
Randolph Voorhies <rand.voorhies@gmail.com>
2017-03-25 14:49:14 -0500

    Working on custom bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 
+import 'custom-bootstrap/build/index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+
 // import './index.css';
 // import '../fonts/glyphicons-halflings-regular.ttf';
 // import 'bootstrap/dist/css/bootstrap.css';
