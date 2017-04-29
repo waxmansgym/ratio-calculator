82293f190fdce868ccc07e2e6313ba112780ef80
Randolph Voorhies <rand.voorhies@gmail.com>
2017-04-29 16:44:51 -0700

    Updating notes

--- src/components/notes.js
+++ src/components/notes.js
@@ -20,20 +20,39 @@ class Notes extends React.Component {
     render() {
         return (
             <div>
-                <Button bsStyle="default" bsSize="large" onClick={this.open}> Notes </Button>
+                <Button bsStyle="default" bsSize="medium" onClick={this.open}> Notes </Button>
 
-                <Modal show={this.state.showModal} onHide={this.close} bsSize="large" aria-labelledby="contained-modal-title-lg">
+                <Modal show={this.state.showModal} onHide={this.close} bsSize="medium" aria-labelledby="contained-modal-title-lg">
                     <Modal.Header closeButton>
                         <Modal.Title>Notes</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <div id="notesbody">
+                            <h3>1. Purpose of this Calculator</h3>
                             <p>
-                                Over years of training athletes, we've found that lift ratios can serve as powerful tools
-                                for evaluating balance and guiding lifter development. By uncovering your biggest
-                                imbalances, you can better prioritize problems and more effectively direct your
-                                programming/training.
+                                This calculator is designed to evaluate a lifter's current balance. 
                             </p>
+
+                            <p>
+                                In our methodology, balance refers to equal development of strength and technique. An
+                                imbalanced lifter either has strength that outpaces his/her technique, technique that is
+                                far superior to his/her strength, or technical abilities in one lift that do not match
+                                his/her technical abilities in another.
+                            </p>
+
+                            <p>
+                                Over years of training athletes, we've found that lift ratios can serve as powerful
+                                tools for evaluating balance and guiding lifter development. By uncovering your biggest
+                                imbalances, you can better prioritize problems, more effectively direct your
+                                programming/training, and improve the likelihood of achieving your goals.
+                            </p>
+
+                            <h3>2. Basis for Analysis</h3>
+                            <p>
+                                The exercise and ratios we use for this calculator are based on information we
+                                summarized in our 2017 article, <a href="http://www.waxmansgym.com/liftratios/">Those Magic Little Lift Ratios</a>.
+                            </p>
+
                         </div>
                     </Modal.Body>
                     <Modal.Footer>
